f86d2683316d8a04a3b6139c21f2a43eacdb15f2
Harsha Kanchina <harsha.kanchina@temenos.com>
2023-01-01 22:29:50 +0530

    checking change push

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Link to="/">home</Link>
+          <Link to="/">Dome</Link>
           <Link to="/otherpage">Other Page</Link>
           <div>
             <Route exact path="/" component={Fib} />
